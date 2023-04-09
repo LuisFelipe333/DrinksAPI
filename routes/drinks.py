0c09a0576b26e7f99e72f348d930883377edf1f5
LuisFelipe333 <luigifelipe333@gmail.com>
2023-04-08 19:18:53 -0600

    Method post for drinks added

--- routes/drinks.py
+++ routes/drinks.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from config.db import conn
 from models.drink import drinks
 from models.drinkType import drinkTypes
+from schemas.drink import Drink
 
 drink = APIRouter()
 
@@ -9,4 +10,6 @@ drink = APIRouter()
 def get_drinks():
     return conn.execute(drinks.select()).fetchall
     
-
+@drink.post("/drinks")
+def create_drink(drink: Drink):
+    print(drink)
