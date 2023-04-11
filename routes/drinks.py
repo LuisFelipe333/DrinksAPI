c9129b6de1de2dcac5398ea5423f788ff194eaed
LuisFelipe333 <luigifelipe333@gmail.com>
2023-04-10 21:12:11 -0600

    Return row when create a insert a drink or drink type

--- routes/drinks.py
+++ routes/drinks.py
@@ -4,6 +4,7 @@ from models.drink import drinks
 from models.drinkType import drinkTypes
 from schemas.drink import Drink
 from schemas.drinkType import DrinkType
+from sqlalchemy import select
 
 drink = APIRouter()
 
@@ -16,7 +17,7 @@ def create_drink(drink: Drink):
     new_drink = {"drink_type_id": drink.drink_type_id, "name": drink.name, "brand": drink.brand, "app_price": drink.app_price, "quantity": drink.quantity}
     result = conn.execute(drinks.insert().values(new_drink))
     conn.commit()
-    return "received"
+    return conn.execute(drinks.select().where(drinks.c.id == result.lastrowid)).first()._mapping
 
 @drink.get("/drink_types")
 def get_drink_types():
@@ -27,4 +28,4 @@ def create_drink_type(drink_type: DrinkType):
     new_drink_type = {"drink_type_name": drink_type.drink_type_name, "cont_alcohol": drink_type.cont_alcohol, "has_brands": drink_type.has_brands}
     result = conn.execute(drinkTypes.insert().values(new_drink_type)) 
     conn.commit()
-    return "received"
+    return conn.execute(drinkTypes.select().where(drinkTypes.c.id == result.lastrowid)).first()._mapping
