7af3f78ccaadc5e628053e5c9e5adb014b05d25e
LuisFelipe333 <luigifelipe333@gmail.com>
2023-04-10 23:07:59 -0600

    Change name relationIngredient file

--- models/relationIngredient.py
+++ models/relationIngredient.py
@@ -1,9 +1,10 @@
 from sqlalchemy import ForeignKey, Table, Column
-from sqlalchemy.sql.sqltypes import String, Integer
+from sqlalchemy.sql.sqltypes import Integer
 from config.db import meta, engine
 from models.drinkType import drinkTypes
+from models.coctail import coctails
 
-drinks = Table("relation_ingredients", meta, Column(
+relationIngredients = Table("relation_ingredients", meta, Column(
     "id", Integer, primary_key=True),
     Column("drink_type_id", Integer, ForeignKey("drink_types.id"), nullable=False),
     Column("coctails_id", Integer, ForeignKey("coctails.id"), nullable=False),
