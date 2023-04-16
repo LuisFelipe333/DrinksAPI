92537e7385f467050aced2b1d7f495e3e817b94e
LuisFelipe333 <luigifelipe333@gmail.com>
2023-04-15 19:44:58 -0600

    Generate tables when run the program

--- models/drink.py
+++ models/drink.py
@@ -2,6 +2,8 @@ from sqlalchemy import ForeignKey, Table, Column
 from sqlalchemy.sql.sqltypes import String, Integer
 from config.db import meta, engine
 from models.drinkType import drinkTypes
+from models.coctail import coctails
+from models.relationIngredient import relationIngredients
 
 
 drinks = Table("drinks", meta, Column(
